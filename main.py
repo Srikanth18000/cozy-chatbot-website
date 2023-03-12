ba32b9fcc38a4f505217bbea22d9666931113478
Srikanth18000 <95920135+Srikanth18000@users.noreply.github.com>
2023-03-12 08:10:56 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from flask import Flask, render_template, request
-from chatgpt import get_response
+from chatgpt import GPT
 
 app = Flask(__name__)
+  # initialize GPT with a unique ID
 
 @app.route("/")
 def home():
@@ -9,9 +10,13 @@ def home():
 
 # Define route for home page
 @app.route("/get", methods=["GET", "POST"])
-def gpt_response():
+def get_response():
+    chat_model = GPT(str(request.values.get("WaId", "")))
+    
+    
     userText = request.args.get('msg')
-    return str(get_response(userText))
+    return str(chat_model.bot(userText))
 
 if __name__ == "__main__":
-    app.run(debug=False)
+    app.run(debug=True)
+
